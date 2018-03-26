3eab7d6a4f1ae6ffdc4e1093966599fcd78bd623
Vivi1183 <viktoria.konovalova91@gmail.com>
2018-03-26 23:26:57 +0200

    Обработаны нажатия кнопок1

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,34 +21,49 @@ public class MainActivity extends AppCompatActivity {
     public void teamAadd3 (View view)
     {
         count_a = count_a + 3;
+        displayA(count_a);
     }
 
     public void teamAadd2 (View view)
     {
         count_a = count_a + 2;
+        displayA(count_a);
     }
 
     public void teamAadd1 (View view)
     {
         count_a = count_a + 1;
+        displayA(count_a);
     }
 
     public void teamBadd3 (View view)
     {
         count_b = count_b + 3;
+        displayB(count_b);
     }
 
     public void teamBadd2 (View view)
     {
         count_b = count_b + 2;
+        displayB(count_b);
     }
     public void teamBadd1 (View view)
     {
         count_b = count_b + 1;
+        displayB(count_b);
     }
     public void reset (View view)
     {
         count_a = 0;
         count_b = 0;
+    }
+    private void displayA(int number) {
+        TextView teamA = (TextView) findViewById(R.id.counter_a_text_view);
+        teamA.setText("" + number);
+    }
+
+    private void displayB(int number) {
+        TextView teamB = (TextView) findViewById(R.id.counter_b_text_view);
+        teamB.setText("" + number);
     }
 }
